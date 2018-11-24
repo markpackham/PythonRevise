29de4440781736db459abb05238fb31da747881e
Mark Packham <markpackham1@gmail.com>
2018-11-24 15:22:43 +0000

    Revise Python https://youtu.be/oy4GOI9vn5M?t=748

---
+++ basic.py
@@ -0,0 +1,21 @@
+#Single line comment
+
+'''
+Multi
+Line
+Comment , can also use """ """
+'''
+
+print('Hello world')
+
+print("""
+Use tripple quotes to print a paragraph that is way to long for regular quotes
+sdasdjasdkasjkdaskjjdkaskdjaskdjaskdjaskdajodoiwdwqoasoidjasoidjasoijdoiajdasoijdasoi
+""")
+
+#print only first char
+print('Hello'[0])
+#print first 3 chars
+print('Hello'[0:3])
+
+print(1,2,3,'Hi')
