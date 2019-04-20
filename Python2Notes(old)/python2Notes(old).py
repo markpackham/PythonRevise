5dcaf3f1efe800b40c3c3c4cb73b3dbe6f41b264
Mark Packham <markpackham1@gmail.com>
2019-04-20 12:41:50 +0100

    old Python 2 notes

---
+++ Python2Notes(old)/python2Notes(old).py
@@ -0,0 +1,47 @@
+#Python 2
+print "Hello Python 2"
+#Python 3
+print("Hello Python 3")
+
+#Following old Python 2
+print "Hello " + "Me"
+
+#float division
+cucumbers = 100
+num_people = 6
+whole_cucumbers_per_person = cucumbers/num_people
+print whole_cucumbers_per_person
+float_cucumbers_per_person = float(cucumbers)/num_people
+print float_cucumbers_per_person
+
+#multi line string
+haiku = """
+The old pond,
+A frog jumps in:
+Plop!"""
+
+#Boolean
+age_is_12 = False
+name_is_maria = True
+
+#To String
+float_1 = 0.25
+float_2 = 40.0
+product = float_1 * float_2
+big_string = "The product was " + str(product)
+
+#Escape character \
+'This isn\'t flying, this is falling with style!'
+
+#String methods
+len()
+lower()
+upper()
+str()
+
+parrot = "Norwegian Blue"
+print len(parrot)
+parrot = "Norwegian Blue"
+print parrot.lower()
+parrot = "norwegian blue"
+print parrot.upper()
