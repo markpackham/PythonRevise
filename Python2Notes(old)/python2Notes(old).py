e32be8fa489bf2524c77786c82fb69f28e1ef8a4
Mark Packham <markpackham1@gmail.com>
2019-04-23 15:15:42 +0100

    lists & dictionaries, looping

--- Python2Notes(old)/python2Notes(old).py
+++ Python2Notes(old)/python2Notes(old).py
@@ -228,10 +228,123 @@ def trip_cost(city, days, spending_money):
   return rental_car_cost(days) + hotel_cost(days) + plane_ride_cost(city) + spending_money
 print trip_cost("Los Angeles", 5, 600)
 
-#Lists aka Arrays
+#List aka Array
 zoo_animals = ["pangolin", "cassowary", "sloth", "dog"];
 if len(zoo_animals) > 3:
   print "The first animal at the zoo is the " + zoo_animals[0]
   print "The second animal at the zoo is the " + zoo_animals[1]
   print "The third animal at the zoo is the " + zoo_animals[2]
   print "The fourth animal at the zoo is the " + zoo_animals[3]
+
+#List assign by Index
+zoo_animals = ["pangolin", "cassowary", "sloth", "tiger"]
+zoo_animals[2] = "hyena"
+zoo_animals[3] = "lion"
+
+#Append & List Length .append & len()
+suitcase = [] 
+suitcase.append("sunglasses")
+suitcase.append("shirt")
+suitcase.append("pants")
+suitcase.append("shoes")
+list_length = len(suitcase) # Set this to the length of suitcase
+print "There are %d items in the suitcase." % (list_length)
+print suitcase
+
+#List Slicing (like getting a Substring)
+suitcase = ["sunglasses", "hat", "passport", "laptop", "suit", "shoes"]
+# The first and second items (index zero and one)
+first = suitcase[0:2]
+# Third and fourth items (index two and three)
+middle = suitcase[2:4]
+# The last two items (index four and five)
+last = suitcase[4:6]
+
+#Remove items from List .remove()
+backpack = ['xylophone', 'dagger', 'tent', 'bread loaf']
+backpack.remove('dagger')
+
+#Insert items into specific order of list
+animals = ["aardvark", "badger", "duck", "emu", "fennec fox"]
+duck_index = animals.index("duck")# Use index() to find "duck"
+animals.insert(duck_index,"cobra")
+print animals # Observe what prints after the insert operation
+
+#For In loop
+my_list = [1,9,3,8,5,7]
+for number in my_list:
+  print 2 * number
+
+#.sort() array
+start_list = [5, 3, 1, 2, 4]
+square_list = []
+for number in start_list:
+  square_list.append(number ** 2)
+square_list.sort()
+print square_list
+#[1, 4, 9, 16, 25]
+
+#Dictionary (just a List with keys)
+# Assigning a dictionary with three key-value pairs to residents:
+residents = {'Puffin' : 104, 'Sloth' : 105, 'Burmese Python' : 106}
+print residents['Puffin'] # Prints Puffin's room number
+print residents['Sloth']
+print residents['Burmese Python']
+
+
+# Dictionary add keys & values
+menu = {} # Empty dictionary
+menu['Chicken Alfredo'] = 14.50 # Adding new key-value pair
+print menu['Chicken Alfredo']
+# Your code here: Add some dish-price pairs to menu!
+menu['Hamburger'] = 8.50
+menu['Pizza Slice'] = 3.50
+menu['Salad'] = 10.00
+print "There are " + str(len(menu)) + " items on the menu."
+print menu
+
+
+# del (delete stuff from Dictionaries, target the key)
+# key - animal_name : value - location 
+zoo_animals = { 'Unicorn' : 'Cotton Candy House',
+'Sloth' : 'Rainforest Exhibit',
+'Bengal Tiger' : 'Jungle House',
+'Atlantic Puffin' : 'Arctic Exhibit',
+'Rockhopper Penguin' : 'Arctic Exhibit'}
+# A dictionary (or list) declaration may break across multiple lines
+# Removing the 'Unicorn' entry. (Unicorns are incredibly expensive.)
+del zoo_animals['Unicorn']
+# Your code here!
+del zoo_animals['Sloth']
+del zoo_animals['Bengal Tiger']
+zoo_animals['Rockhopper Penguin'] = 'Plains Exhibit'
+print zoo_animals
+
+
+#Dictionary manipulation example
+inventory = {
+  'gold' : 500,
+  'pouch' : ['flint', 'twine', 'gemstone'], # Assigned a new list to 'pouch' key
+  'backpack' : ['xylophone','dagger', 'bedroll','bread loaf']
+}
+# Adding a key 'burlap bag' and assigning a list to it
+inventory['burlap bag'] = ['apple', 'small ruby', 'three-toed sloth']
+# Sorting the list found under the key 'pouch'
+inventory['pouch'].sort() 
+inventory['pocket'] = ['seashell', 'strange berry', 'lint']
+inventory['backpack'].sort()
+inventory['backpack'].remove('dagger')
+inventory['gold'] = inventory['gold'] + 50
+
+
+#String Looping
+for letter in "Codecademy":
+  print letter 
+# Empty lines to make the output pretty
+print
+print
+word = "Programming is fun!"
+for letter in word:
+  # Only print out the letter i
+  if letter == "i":
+    print letter
